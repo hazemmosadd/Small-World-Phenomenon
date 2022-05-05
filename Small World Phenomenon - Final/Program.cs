71123323825d667711f7da6b9303610d6d5285a7
hazemmosadd <76247850+hazemmosadd@users.noreply.github.com>
2022-05-06 01:51:17 +0200

    Add project files.

---
+++ Small World Phenomenon - Final/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Small_World_Phenomenon
+{
+    class Program
+    {
+        static string moviesPath = "", queriesPath = "" , solutionPath = "" , sl;
+        static Queue<string > answers = new Queue<string>();
+
+
+        public static void parseSolution()
+        {
+            StreamReader reader = new StreamReader(solutionPath);
+            string answer = "";
+            while (true)
+            {
+                string line = reader.ReadLine();
+                if (line == null)
+                    break;
+                if (line == "")
+                {
+                    answers.Enqueue(answer);
+                    answer = "";
+                }
+                else
+                {
+                    answer += line + "\n";
+                }
+
+            }
+            Console.WriteLine("Solution Parsing Done, {0} answers", answers.Count());
+        }
+        static void menu()
+        {
+            Console.WriteLine("==========================");
+            Console.WriteLine("     CHOOSE TESTCASE");
+            Console.WriteLine("==========================");
+            Console.WriteLine(" ");
+            Console.WriteLine("   ------   Small    --------");
+
+            Console.WriteLine(" 1  - 139 Movies 110 Quieries");
+            Console.WriteLine(" 2  - 187 Movies 50 Quieries");
+            Console.WriteLine(" ");
+            Console.WriteLine("   ------   Medium    --------");
+            Console.WriteLine(" 3  - 967 Movies 85 Quieries");
+            Console.WriteLine(" 4  - 967 Movies 4000 Quieries");
+            Console.WriteLine(" 5  - 4736 Movies 110 Quieries");
+            Console.WriteLine(" 6  - 4736 Movies 2000 Quieries");
+            Console.WriteLine(" ");
+            Console.WriteLine("   ------   Large    --------");
+            Console.WriteLine(" 7  - 14129 Movies 26 Quieries");
+            Console.WriteLine(" 8  - 14129 Movies 600 Quieries");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("   ------  Extreme   --------");
+            Console.WriteLine(" 9  - 122806 Movies 22 Quieries");
+            Console.WriteLine(" 10 - 122806 Movies 200 Quieries");
+
+
+
+            int choice = 0;
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    moviesPath = @"small\Case1\Movies193.txt";
+                    queriesPath = @"small\Case1\queries110.txt";
+                    sl = @"small\Case1\queries110 - Solution.txt";
+                    break;
+
+                case 2:
+                    moviesPath = @"small\Case2\Movies187.txt";
+                    queriesPath = @"small\Case2\queries50.txt";
+                    sl = @"small\Case2\queries50 - Solution.txt";
+                    break;
+
+                case 3:
+                    moviesPath = @"medium\Case1\Movies967.txt";
+                    queriesPath = @"medium\Case1\queries85.txt";
+                    sl = @"medium\Case1\Solutions\queries85 - Solution.txt";
+                    break;
+
+                case 4:
+                    moviesPath = @"medium\Case1\Movies967.txt";
+                    queriesPath = @"medium\Case1\queries4000.txt";
+                    sl = @"medium\Case1\Solutions\queries4000 - Solution.txt";
+                    break;
+
+                case 5:
+                    moviesPath = @"medium\Case2\Movies4736.txt";
+                    queriesPath = @"medium\Case2\queries110.txt";
+                    sl = @"medium\Case2\Solutions\queries110 - Solution.txt";
+                    break;
+                case 6:
+                    moviesPath = @"medium\Case2\Movies4736.txt";
+                    queriesPath = @"medium\Case2\queries2000.txt";
+                    sl = @"medium\Case2\Solutions\queries2000 - Solution.txt";
+                    break;
+
+                case 7:
+                    moviesPath = @"large\Movies14129.txt";
+                    queriesPath = @"large\queries26.txt";
+                    sl = @"\large\Solutions\queries26 - Solution.txt";
+                    break;
+                case 8:
+                    moviesPath = @"large\Movies14129.txt";
+                    queriesPath = @"large\queries600.txt";
+                    sl = @"\large\Solutions\queries600 - Solution.txt"; 
+                    break;
+                case 9:
+                    moviesPath = @"extreme\Movies122806.txt";
+                    queriesPath = @"extreme\queries22.txt";
+                    sl = @"extreme\Solutions\queries22 - Solution.txt";
+                    
+
+                    break; 
+                case 10:
+                    moviesPath = @"extreme\Movies122806.txt";
+                    queriesPath = @"extreme\queries200.txt";
+                    sl = @"extreme\Solutions\queries200 - Solution.txt";
+                    break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            menu();
+
+             solutionPath = @"C:\Users\LENOVO\source\repos\Small World Phenomenon\Small World Phenomenon\Testcases\Complete\" + sl; 
+            parseSolution();
+
+
+
+            string[] lines = File.ReadAllLines(@"C:\Users\LENOVO\source\repos\Small World Phenomenon\Small World Phenomenon\Testcases\Complete\" + moviesPath, Encoding.UTF8);
+            List<List<string>> MovieActors = new List<List<string>>();
+            Dictionary<string, HashSet<string>> Actor_Movies = new Dictionary<string, HashSet<string>>();
+            PriorityQueue<string, int> queue = new PriorityQueue<string, int>();
+            int x = 0;
+            foreach (var line in lines)
+            {
+
+                var Line = line.Split("/");
+                MovieActors.Add(new List<string>());
+                for (int i = 1; i < Line.Length; i++)
+                {
+                    MovieActors[x].Add(Line[i]);
+                    if (!Actor_Movies.ContainsKey(Line[i]))
+                        Actor_Movies.Add(Line[i], new HashSet<string>());
+                    Actor_Movies[Line[i]].Add(Line[0]);
+                }
+                x++;
+            }
+
+            string[] Qlines = File.ReadAllLines(@"C:\Users\LENOVO\source\repos\Small World Phenomenon\Small World Phenomenon\Testcases\Complete\" + queriesPath, Encoding.UTF8);
+            var queries = new List<KeyValuePair<string, string>>();
+            foreach (var line in Qlines)
+            {
+                string[] lineParts = line.Split('/');
+                queries.Add(new KeyValuePair<string, string>(lineParts[0], lineParts[1]));
+            }
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            smallWorld.solve(MovieActors, queries, Actor_Movies , answers);
+            stopwatch.Stop();
+            Console.WriteLine("Elapsed Time : " +(float) stopwatch.ElapsedMilliseconds / 1000  +" second");
+
+
+
+
+
+        }
+    }
+}
---
+++ Small World Phenomenon - Final/smallWorld.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Math;
+
+namespace Small_World_Phenomenon
+{
+    class smallWorld
+    {
+        static string src ="", dest="";
+        static Dictionary<string, string> parent = new Dictionary<string, string>();
+
+        public static Dictionary<string, List<string>> makeTheGraph(List<List<string>> MovieActors)
+        {
+            Dictionary<string, List<string>> adjList = new Dictionary<string, List<string>>();
+            foreach (var movie in MovieActors)
+            {
+                for (int i = 0; i < movie.Count; i++)
+                {
+                    if (!adjList.ContainsKey(movie[i]))
+                        adjList.Add(movie[i], new List<string>());
+
+                    for (int j = i + 1; j < movie.Count; j++)
+                    {
+                        if (!adjList.ContainsKey(movie[j]))
+                            adjList.Add(movie[j], new List<string>());
+
+                        if (!adjList[movie[i]].Contains(movie[j]))
+                            adjList[movie[i]].Add(movie[j]);
+                        if (!adjList[movie[j]].Contains(movie[i]))
+                            adjList[movie[j]].Add(movie[i]);
+
+
+                    }
+                }
+            }
+            return adjList;
+        }
+        public static List<string> getBestPathFinal(Dictionary<string, List<string>> adjList, Dictionary<string, HashSet<string>> ActorMoviesList)
+        {
+            Queue<string> pqNext = new Queue<string>();
+            Queue<string> pq = new Queue<string>();
+            Dictionary<string, bool> visited = new Dictionary<string, bool>();
+            Dictionary<string, int> Strength = new Dictionary<string, int>();
+            Dictionary<string, int> level = new Dictionary<string, int>();
+
+
+            pq.Enqueue(src);
+            parent[src] = "-1";
+            level[src] = 0;
+            Strength[src] = 0;
+            bool found = false;
+
+
+            while (pq.Count != 0)
+            {
+                string node = pq.Dequeue();
+                visited[node] = true;
+                foreach (string child in adjList[node])
+                {
+   
+                    if (!level.ContainsKey(child))
+                        level[child] = 9999;
+                    if (!visited.ContainsKey(child))
+                        visited[child] = false;
+                    if (!Strength.ContainsKey(child))
+                        Strength[child] = -9999;
+
+                    if (child == dest)
+                        found = true;
+                   
+                    if (!visited[child] && (level[child] > level[node]))
+                    {
+
+                        level[child] = level[node] + 1;
+                        int strength = CalcStrength(node, child, ActorMoviesList) + Strength[node];
+                        if (strength > Strength[child])
+                        {
+                            Strength[child] = strength;
+                            parent[child] = node;
+                        }
+                        pqNext.Enqueue(child);
+
+                    }
+                }
+                if (pq.Count == 0)
+                {
+                    pq = pqNext;
+                    pqNext = new Queue<string>();
+
+                    if (found)
+                        break;
+                }
+            }
+            List<string> path = new List<string>();
+            String cur = dest;
+            while (cur != "-1")
+            {
+                path.Add(cur);
+                cur = parent[cur];
+
+            }
+            path.Reverse();
+
+
+            return path;
+
+
+
+
+
+        }
+        public static int CalcStrength(string a, string b, Dictionary<string, HashSet<string>> ActorMoviesList)
+        {
+            List<string> movies = new List<string>();
+            string A = a, B = b;
+            if (ActorMoviesList[a].Count > ActorMoviesList[b].Count)
+            {
+                A = b;
+                B = a;
+            }
+
+            foreach (var movie in ActorMoviesList[A])
+            {
+
+                if (ActorMoviesList[B].Contains(movie))
+                    movies.Add(movie);
+            }
+            return movies.Count;
+        }
+        public static List<List<string>> getAllMoviesInPath(List<string> path, Dictionary<string, HashSet<string>> ActorMoviesList)
+        {
+
+            int relationStrength = 0;
+            List<List<string>> moviesInPath = new List<List<string>>();
+            for (int i = 1; i < path.Count; i++)
+            {
+                List<string> moviesBetweenTwoActors = containsBoth(path[i], path[i - 1], ActorMoviesList);
+                moviesInPath.Add(moviesBetweenTwoActors);
+            }
+            return moviesInPath;
+        }
+        public static List<string> containsBoth(string a, string b, Dictionary<string, HashSet<string>> ActorMoviesList)
+        {
+            List<string> movies = new List<string>();
+            string A = a, B = b;
+            if (ActorMoviesList[a].Count > ActorMoviesList[b].Count)
+            {
+                A = b; B = a;
+            }
+
+            foreach (var movie in ActorMoviesList[A])
+            {
+                if (ActorMoviesList[B].Contains(movie))
+                    movies.Add(movie);
+
+            }
+            return movies;
+        }
+        public static string answerString(List<List<string>> moviesInPath, List<string> BestPath)
+        {
+            //calc rs 
+            int RS = 0;
+            string chainOfMovies = "";
+            string chainOfActors = ""; 
+            foreach (var ListOfMovies in moviesInPath)
+            {
+                RS += ListOfMovies.Count;
+                chainOfMovies += ListOfMovies[0] + " => ";
+            }
+            StringBuilder sb = new StringBuilder(chainOfMovies);
+            sb.Remove(chainOfMovies.Length-1, 1);
+            chainOfMovies = sb.ToString();
+
+
+            int DoS = BestPath.Count - 1;
+            for (int i =0; i < BestPath.Count-1; i++)
+            {
+                chainOfActors+=BestPath[i]+ " -> ";
+
+            }
+            chainOfActors += BestPath[BestPath.Count - 1];
+            string answer = ""; 
+            answer+=src+"/"+dest+"\n"+"DoS = "+DoS+", RS = "+RS+"\n"+ "CHAIN OF ACTORS: "+chainOfActors+"\n" + "CHAIN OF MOVIES:  => "+ chainOfMovies+"\n" ;
+            return answer; 
+
+
+        }
+        public static void solve(List<List<string>> MovieActors, List<KeyValuePair<string, string>> queries, Dictionary<string, HashSet<string>> ActorMoviesList , Queue<string> Answers )
+        {
+
+            Dictionary<string, List<string>> adjList = makeTheGraph(MovieActors);
+            int i = 1;
+
+            List<string> path;
+            List<List<string>> MoviesInPath;
+
+            foreach (var query in queries)
+            {
+                src = query.Key; dest = query.Value;
+                path = getBestPathFinal(adjList, ActorMoviesList);
+                MoviesInPath = getAllMoviesInPath(path, ActorMoviesList);
+                string ans = answerString(MoviesInPath, path);
+                string expected = Answers.Dequeue();
+                if (ans == expected)
+                {
+                    Console.WriteLine("Test #" + i + "Passed!");
+                }
+                else
+                {
+                    Console.WriteLine("------------------------------------");
+                    Console.WriteLine("Wrong answer on Test #" + i);
+                    Console.WriteLine("Output :");
+                    Console.WriteLine(ans);
+                    Console.WriteLine();
+                    Console.WriteLine("Expected Output :");
+                    Console.WriteLine(expected);
+                    Console.WriteLine("------------------------------------");
+
+                }
+           
+                i++;
+            }
+        }
+
+    }
+}
+
